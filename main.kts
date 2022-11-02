c28c33aafe6c9593cde124e704c23d90cacd3c99
Marvish Subhas Chandra <mschandr@uci.edu>
2022-11-02 08:32:16 -0700

    Add files via upload

---
+++ main.kts
@@ -0,0 +1,12 @@
+// Marvish Chandra
+
+class calculateBMI:
+    fun heightWeight(m,w){
+    givenHeight = m * m
+    givenWeight = w
+    }
+    fun findBMI(){
+    myBMI = (givenWeight / givenHeight)
+    println(myBMI)
+    }
+
